2750e3c76d15390aa01325420d8c6c5365df353e
harryquimbay <hquimbay@byupathway.edu>
2026-09-03 22:51:39 -0500

    Compelted Ex02

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,35 @@
-using System;
 
-class Program
+// ask for the grade
+Console.Write("What is your grade percentage? ");
+string userInput = Console.ReadLine();
+int grade = int.Parse(userInput);
+
+//variable
+string letter;
+
+
+//Conditional to assign the grade in letters
+if (grade >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+    letter = "A";
 }
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+{
+    Console.WriteLine($"Your grade is {letter}."); 
+}
+
