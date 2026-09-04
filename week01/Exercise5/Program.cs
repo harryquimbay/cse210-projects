53f4e70e40753c1ec64273cf1cf38a2cb6817944
harryquimbay <hquimbay@byupathway.edu>
2026-09-03 23:14:20 -0500

    completed ex05

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,51 @@
-using System;
 
-class Program
+//Welcome sms
+static void DisplayWelcome()
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
+    Console.WriteLine("Welcome to the Program!");
 }
+
+
+//Ask Name
+static string PromptUserName()
+{
+    Console.Write("Please enter your name: ");
+    string name = Console.ReadLine();
+    return name;
+}
+
+
+//Ask fav number
+static int PromptUserNumber()
+{
+    Console.Write("Please enter your favorite number: ");
+    string userInput = Console.ReadLine();
+    int number = int.Parse(userInput);
+    return number;
+}
+
+
+
+//Square of the #
+static int SquareNumber(int number)
+{
+    int square = number * number;
+    return square;
+}
+
+
+//Show result
+static void DisplayResult(string name, int square)
+{
+    Console.WriteLine($"{name}, the square of your number is {square}");
+}
+
+
+//Function to save result
+DisplayWelcome();
+
+string userName = PromptUserName();
+int userNumber = PromptUserNumber();
+int squaredNumber = SquareNumber(userNumber);
+
+DisplayResult(userName, squaredNumber);
