9c51bba38af65f57462838cffc6a18bd7fa40844
harryquimbay <hquimbay@byupathway.edu>
2026-09-03 23:09:50 -0500

    completed ex04

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
-using System;
+using System.Collections.Generic;
 
-class Program
+Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+List<int> numbers = new List<int>();
+
+int number = -1;
+
+while (number != 0)
 {
-    static void Main(string[] args)
+    Console.Write("Enter number: ");
+    string userInput = Console.ReadLine();
+    number = int.Parse(userInput);
+
+    if (number != 0)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        numbers.Add(number);
     }
 }
+
+// sum
+int sum = 0;
+
+foreach (int value in numbers)
+{
+    sum += value;
+}
+
+// average
+double average = (double)sum / numbers.Count;
+
+//largest number
+int largest = numbers[0];
+
+foreach (int value in numbers)
+{
+    if (value > largest)
+    {
+        largest = value;
+    }
+}
+
+// Show results
+Console.WriteLine($"The sum is: {sum}");
+Console.WriteLine($"The average is: {average}");
+Console.WriteLine($"The largest number is: {largest}");
